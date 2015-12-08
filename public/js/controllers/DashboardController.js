7d815479dd8caa0dae359437ad36416285f2fcb5
Sufiane <su.souissi@gmail.com>
2015-12-08 18:44:58 +0100

    adding newSugegstion to admin dashboard
    adding newSuggestion to admin dashboard, still need work on css (bug
    with footer) and redirection to actual newSuggestion page etc...

--- public/js/controllers/DashboardController.js
+++ public/js/controllers/DashboardController.js
@@ -1,11 +1,15 @@
 angular.module('L3C_V2')
     .controller('DashboardController', function($scope, $location, SessionService, SuggestionService){
 
+    
+    $scope.admin = false;
+    
     SessionService.getCurrentSession().then(function(response){
 
         $scope.session = response.data;
 
         if($scope.session.isAdmin){
+            $scope.admin = true;
             $scope.title = "Bienvenue, "+ $scope.session.name;
 
         }else{
@@ -16,19 +20,20 @@ angular.module('L3C_V2')
     SuggestionService.getCurrentSuggestion().then(function(response){
 
         $scope.suggestion = response.data.data;
-
     });
-
 
     SuggestionService.getAllSuggestion().then(function(response){
         
         $scope.penultimateSuggestion = response.data[response.data.length-2];
         $scope.movies = response.data;
-     
     });
 
     $scope.go = function(path){
         $location.path(path);
     };
 
+    $scope.alert = function(text){
+        window.alert(text);
+    };
+    
 });
