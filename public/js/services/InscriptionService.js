7d0266c8fff5f37607f4ff31c8c0f3a7a27181e9
Sufiane <su.souissi@gmail.com>
2015-12-13 20:45:48 +0100

    New InscriptionService + Controller
    migrating from ajax to $http post.

---
+++ public/js/services/InscriptionService.js
@@ -0,0 +1,32 @@
+angular.module('L3C_V2')
+    .service('InscriptionService', function($http){
+
+
+    var processInscription = function(data){
+        var url = "/newMember";
+        var responseService = {
+            error : "",
+            errorMessage : "",
+            test: ""
+        };
+
+        return $http.post(url, data)
+            .success(function(response){
+                if(response.codeResponse === "ko"){
+                    responseService.error = response.codeResponse;
+                    responseService.errorMessage = response.message;
+                    
+                    return responseService;
+                }else{
+                    return response.data.data;
+                }
+            })
+            .error(function(response){
+                return response.data.data;
+            });
+    };
+
+    return {
+        processInscription : processInscription
+    };  
+});
