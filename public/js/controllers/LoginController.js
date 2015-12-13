c195d3323fcd6a0204a3e6c2cccce4b759eca723
Sufiane <su.souissi@gmail.com>
2015-12-13 20:49:00 +0100

    New service : LoginService
    migrating ajax post to $http post.

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -1,30 +1,16 @@
 angular.module('L3C_V2')
-    .controller('LoginController', function($scope,$location){
+    .controller('LoginController', function($scope,$location, LoginService){
     $scope.title = "Login"; 
     
     $scope.formData = {};
     
     $scope.processLogin = function(){
-        alert('faire un service pour gerer la connection!!!!');
-        $.ajax({
-            type:'POST',
-            url:'/loginConnection',
-            data: $scope.formData,
-            async:false,
-            success:function(response){
-                
-                if(response.codeResponse === "ko"){
-                    $scope.error = "true";
-                    $scope.errorMessage = response.message;
-                    
-                }else{
-                    alert(response.message);
-                    alert('utiliser location vers la suggestion ou panel admin/member');
-                    $location.path("/dashboard");
-                }
-            },
-            error:function(){
-                console.error('Error adding newMember!! :( ');
+        LoginService.processLogin($scope.formData).then(function(response){
+            if(response.codeResponse === "ko"){
+                $scope.error = "true";
+                $scope.errorMessage = response.message;
+            }else{
+                $location.path('/dashboard');
             }
         });
     };
---
+++ public/js/services/LoginService.js
@@ -0,0 +1,31 @@
+angular.module('L3C_V2')
+    .service('LoginService', function($http){
+
+
+    var processLogin = function(data){
+        var url = "/loginConnection";
+        var responseService = {
+            error: "",
+            errorMessage: ""
+        };
+
+        return $http.post(url, data)
+            .success(function(response){
+                if(response.codeResponse === "ko"){
+                    responseService.error = response.codeResponse;
+                    responseService.errorMessage = response.message;
+
+                    return responseService;
+                }else{
+                    return response.data.data;
+                }
+            })
+            .error(function(response){
+                return response.data.data;
+            });
+    };
+    
+    return {
+        processLogin : processLogin
+    };
+});
