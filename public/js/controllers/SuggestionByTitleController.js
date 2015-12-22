6eacd593c7d6eeec0299922a5bbf40afa17facbd
Sufiane <su.souissi@gmail.com>
2015-12-22 22:37:00 +0100

    fixing correct name SuggestionByTitleController

--- public/js/controllers/SuggestionByTitleController.js
+++ public/js/controllers/SuggestionByTitleController.js
@@ -1,6 +1,8 @@
 angular.module('L3C_V2')
-    .controller('SuggestionByTitle', function($scope, $routeParams, SuggestionService){
+    .controller('SuggestionByTitleController', function($scope, $routeParams, SuggestionService){
    
+    
+    console.warn($routeParams.title);
     SuggestionService.getSuggestionByTitle($routeParams.title).then(function(response){
         console.log(response);
         console.log($routeParams.title);
