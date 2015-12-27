0b2b21a793a662b1b808d088bd46d49061111e89
Sufiane <su.souissi@gmail.com>
2015-12-27 14:24:30 +0100

    handling server response + deleting useless console

--- public/js/controllers/SuggestionByTitleController.js
+++ public/js/controllers/SuggestionByTitleController.js
@@ -1,11 +1,7 @@
 angular.module('L3C_V2')
     .controller('SuggestionByTitleController', function($scope, $routeParams, SuggestionService){
    
-    
-    console.warn($routeParams.title);
     SuggestionService.getSuggestionByTitle($routeParams.title).then(function(response){
-        console.log(response);
-        console.log($routeParams.title);
+        $scope.suggestion = response.data.data;
     }); 
-    
 });
