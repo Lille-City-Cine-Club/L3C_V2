48007879fe0faf5390b28686436844837ad08322
Sufiane <su.souissi@gmail.com>
2016-01-04 15:07:30 +0100

    New directive for file upload

---
+++ public/js/directives/fileUpload.js
@@ -0,0 +1,18 @@
+angular.module('L3C_V2')
+    .directive('fileUpload', function($parse){
+
+    return{
+        restrict: 'A',
+        link: function(scope, element, attrs){
+            var model = $parse(attrs.fileUpload);
+            var modelSetter = model.assign;
+            
+            element.bind('change', function(){
+               scope.$apply(function(){
+                   modelSetter(scope, element[0].files[0]);
+               }) 
+            });
+        }
+    };
+
+});
