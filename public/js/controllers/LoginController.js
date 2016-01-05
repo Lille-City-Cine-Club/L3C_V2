8ffc603b52f587973f6fade4d7c19f11f84fd3b8
Sufiane <su.souissi@gmail.com>
2016-01-05 14:23:37 +0100

    fixing redirection problem when an error occur on connection
    the error message is now correctly displayed and there is no useless
    redirection to the homepage made

--- public/js/controllers/LoginController.js
+++ public/js/controllers/LoginController.js
@@ -1,14 +1,18 @@
 angular.module('L3C_V2')
     .controller('LoginController', function($scope,$location, LoginService){
-    $scope.title = "Login"; 
     
+    $scope.title = "Login"; 
     $scope.formData = {};
+   
     
     $scope.processLogin = function(){
-        LoginService.processLogin($scope.formData).then(function(response){
+        LoginService.processLogin($scope.formData).then(function(responseService){
+            var response = responseService.data;
+            
             if(response.codeResponse === "ko"){
                 $scope.error = "true";
                 $scope.errorMessage = response.message;
+                
             }else{
                 $location.path('/dashboard');
             }
