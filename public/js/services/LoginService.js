1ed1b33557bb143d1742a2d782060b223494773e
Sufiane <su.souissi@gmail.com>
2016-01-05 14:18:24 +0100

    deleting useless condition and object
    there is no need to do any logic here

--- public/js/services/LoginService.js
+++ public/js/services/LoginService.js
@@ -4,21 +4,10 @@ angular.module('L3C_V2')
 
     var processLogin = function(data){
         var url = "/loginConnection";
-        var responseService = {
-            error: "",
-            errorMessage: ""
-        };
 
         return $http.post(url, data)
             .success(function(response){
-                if(response.codeResponse === "ko"){
-                    responseService.error = response.codeResponse;
-                    responseService.errorMessage = response.message;
-
-                    return responseService;
-                }else{
-                    return response;
-                }
+                return response.data;   
             })
             .error(function(response){
                 return response.data.data;
