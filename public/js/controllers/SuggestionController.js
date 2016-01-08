28898beeba4538ca2ba3f6d27edf92c2f1ff29e5
Sufiane <su.souissi@gmail.com>
2016-01-08 12:35:51 +0100

    fixing the trailer pb + improving the functions used to toggle it

--- public/js/controllers/SuggestionController.js
+++ public/js/controllers/SuggestionController.js
@@ -1,11 +1,11 @@
 angular.module('L3C_V2')
     .controller('SuggestionController', function($scope, $location, $sce, SuggestionService){
 
-    
+
     $scope.showingTrailer = false;
 
     SuggestionService.getCurrentSuggestion().then(function(response){
-        
+
         $scope.response = response.data;
 
         if($scope.response.codeResponse === "ko"){
@@ -14,12 +14,15 @@ angular.module('L3C_V2')
             $scope.suggestion = $scope.response.data;
         }
     });
-    
-    $scope.showTrailer = function(){
-                
-        $scope.trailerUrl = $sce.trustAsResourceUrl('https://www.youtube.com/embed/'+$scope.suggestion.trailer);
-        $scope.showingTrailer = true;
-                    
+
+    $scope.toggleTrailer = function(hide){
+
+        if(!hide){
+            $scope.trailerUrl = $sce.trustAsResourceUrl('https://www.youtube.com/embed/'+$scope.suggestion.trailer);
+            $scope.showingTrailer = true;
+        }else{
+            $scope.showingTrailer = false;
+        }
     };
-    
+
 });
