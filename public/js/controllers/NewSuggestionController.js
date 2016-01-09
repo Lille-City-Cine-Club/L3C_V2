2a676bff303b6e7924f4ceb6b2a13e5c97c53ea4
Sufiane <su.souissi@gmail.com>
2016-01-09 20:10:31 +0100

    to display success/error message when sending new suggestion

--- public/js/controllers/NewSuggestionController.js
+++ public/js/controllers/NewSuggestionController.js
@@ -1,8 +1,33 @@
 angular.module('L3C_V2')
-    .controller('NewSuggestionController', function($scope){
-    
-    
+    .controller('NewSuggestionController', function($scope, $timeout, SuggestionService){
+
+    $scope.formData = {};
+
+    var reset = angular.copy($scope.formData);
+
     $scope.processNewSuggestion = function(){
         window.alert('processNewSuggestion here!!');
+        console.info($scope.formData);
+
+        var poster = $scope.poster;
+
+        SuggestionService.setSuggestion($scope.formData, poster).then(function(response){
+            if(response.data.codeResponse === "ko"){
+                $scope.error = response.data.message;
+                $scope.errorMessage = response.data.message;
+            }else{
+                $scope.error = '';
+                $scope.success = response.data.message;
+                $scope.successMessage = response.data.message;
+                
+                $scope.formData = reset; 
+                $scope.formNewSuggestion.$setPristine();
+                
+                $timeout(function(){
+                    $scope.success = '';
+                    
+                }, 2000);
+            }
+        });
     };
 });
